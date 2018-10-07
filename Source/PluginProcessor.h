d103112dedae7108c7819db725becb328bec727d
Nachiketa Gargi <nachiketa.gargi@gmail.com>
2018-10-06 20:15:47 -0400

    Basic structure of project

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -10,52 +10,62 @@
 
 #pragma once
 
+#include <queue>
 #include "../JuceLibraryCode/JuceHeader.h"
 
 //==============================================================================
 /**
-*/
-class MusicmakeathonAudioProcessor  : public AudioProcessor
-{
-public:
-    //==============================================================================
-    MusicmakeathonAudioProcessor();
-    ~MusicmakeathonAudioProcessor();
+ */
+class MusicmakeathonAudioProcessor : public AudioProcessor {
+ public:
+  //==============================================================================
+  MusicmakeathonAudioProcessor();
+  ~MusicmakeathonAudioProcessor();
 
-    //==============================================================================
-    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
-    void releaseResources() override;
+  //==============================================================================
+  void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+  void releaseResources() override;
 
-   #ifndef JucePlugin_PreferredChannelConfigurations
-    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
-   #endif
+#ifndef JucePlugin_PreferredChannelConfigurations
+  bool isBusesLayoutSupported(const BusesLayout& layouts) const override;
+#endif
 
-    void processBlock (AudioBuffer<float>&, MidiBuffer&) override;
+  void processBlock(AudioBuffer<float>&, MidiBuffer&) override;
 
-    //==============================================================================
-    AudioProcessorEditor* createEditor() override;
-    bool hasEditor() const override;
+  //==============================================================================
+  AudioProcessorEditor* createEditor() override;
+  bool hasEditor() const override;
 
-    //==============================================================================
-    const String getName() const override;
+  //==============================================================================
+  const String getName() const override;
 
-    bool acceptsMidi() const override;
-    bool producesMidi() const override;
-    bool isMidiEffect() const override;
-    double getTailLengthSeconds() const override;
+  bool acceptsMidi() const override;
+  bool producesMidi() const override;
+  bool isMidiEffect() const override;
+  double getTailLengthSeconds() const override;
 
-    //==============================================================================
-    int getNumPrograms() override;
-    int getCurrentProgram() override;
-    void setCurrentProgram (int index) override;
-    const String getProgramName (int index) override;
-    void changeProgramName (int index, const String& newName) override;
+  //==============================================================================
+  int getNumPrograms() override;
+  int getCurrentProgram() override;
+  void setCurrentProgram(int index) override;
+  const String getProgramName(int index) override;
+  void changeProgramName(int index, const String& newName) override;
 
-    //==============================================================================
-    void getStateInformation (MemoryBlock& destData) override;
-    void setStateInformation (const void* data, int sizeInBytes) override;
+  //==============================================================================
+  void getStateInformation(MemoryBlock& destData) override;
+  void setStateInformation(const void* data, int sizeInBytes) override;
 
-private:
-    //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MusicmakeathonAudioProcessor)
+ private:
+  //==============================================================================
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MusicmakeathonAudioProcessor)
+  static const int fftOrder = 10;
+  static const int bufferSize = 1 << fftOrder;
+  std::queue<float> inputFifo[bufferSize];
+  float fftData[2 * bufferSize];
+  std::queue<float> sampleBufferFifo[bufferSize];
+  dsp::FFT forwardFFT;
+  bool nextFFTBlockReady;
+  bool nextOutputReady;
+    void findAndLoadSample(std::queue<float>[bufferSize], std::queue<float>[bufferSize]);
+
 };
